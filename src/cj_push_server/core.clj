f96807fa9ab873a888d561d20d756dea1baf75d4
Benjamin Eberlei <kontakt@beberlei.de>
2012-10-03 22:20:39 +0200

    Move push code into deftype

--- src/cj_push_server/core.clj
+++ src/cj_push_server/core.clj
@@ -71,87 +71,93 @@
   (distribute-feed [this feed])
 )
 
-;(deftype PostgreSQLPushServer [] PushServer
+(deftype PostgreSQLPushServer [] PushServer
+  (create-topic [this topic]
+    (sql/insert-values :topics [:topic :requires_fetching] [topic 1])
+    (let [id (postgres-last-insert-id "topics" "id")]
+      {:id id :topic topic :requires_fetching 1 :last_fetched_at nil}))
 
-(defn push-create-topic [topic]
-  (sql/insert-values :topics [:topic :requires_fetching] [topic 1])
-  (let [id (postgres-last-insert-id "topics" "id")]
-    {:id id :topic topic :requires_fetching 1 :last_fetched_at nil}))
+  (get-topic [this topic]
+    (sql/with-query-results results ["SELECT * FROM topics WHERE topic = ?" topic]
+                            (first results)))
 
-(defn push-get-topic [topic]
-  (sql/with-query-results results ["SELECT * FROM topics WHERE topic = ?" topic]
-                          (first results)))
+  (get-or-create-topic [this topic]
+    (doto (new java.net.URL topic) (.toURI))
+      (let [row (get-topic this topic)]
+        (if (= row nil)
+          (create-topic this topic)
+          (do row))))
 
-(defn push-get-or-create-topic [topic]
-  (doto (new java.net.URL topic) (.toURI))
-    (let [row (push-get-topic topic)]
-      (if (= row nil)
-        (push-create-topic topic)
-        (do row))))
+  (mark-topic-fetched [this feed]
+    (let [now (date/now)]
+      (sql/update-values :topics ["id = ?" (:id feed)] {:last_fetched_at (postgres-date now)})))
 
-(defn push-mark-feed-fetched [feed]
-  (let [now (date/now)]
-    (sql/update-values :topics ["id = ?" (:id feed)] {:last_fetched_at (postgres-date now)})))
+  (subscription-exists? [this topic_id callback]
+    (sql/with-query-results results ["SELECT count(*) AS exists FROM subscriptions WHERE topic_id = ? AND callback = ?" 
+                                     topic_id
+                                     callback]
+                            (let [exists (:exists (first results))]
+                              (= exists 1))))
 
-(defn push-subscription-exists? [topic_id callback]
-  (sql/with-query-results results ["SELECT count(*) AS exists FROM subscriptions WHERE topic_id = ? AND callback = ?" 
-                                   topic_id
-                                   callback]
-                          (let [exists (:exists (first results))]
-                            (= exists 1))))
-
-(defn push-create-subscription [topic_id hub]
-  (let [callback (:callback hub)]
-    (sql/insert-values :subscriptions
-                       [:callback :topic_id]
-                       [(cast String callback) topic_id]
-                       )))
+  (create-subscription [this topic_id hub]
+    (let [callback (:callback hub)]
+      (sql/insert-values :subscriptions
+                         [:callback :topic_id]
+                         [(cast String callback) topic_id]
+                         )))
                              
-(defn push-delete-subscription [topic_id hub]
-  (let [callback (:callback hub)]
-    (sql/delete-rows :subscriptions ["callback = ? AND topic_id = ?" callback topic_id])))
+  (delete-subscription [this topic_id hub]
+    (let [callback (:callback hub)]
+      (sql/delete-rows :subscriptions ["callback = ? AND topic_id = ?" callback topic_id])))
 
-(defn push-challenge-subscription [topic_id hub is_subscription success failure]
-  (with-open [client (http/create-client)] ; Create client
-    (doto (new java.net.URL (:topic hub)) (.toURI))
-    (doto (new java.net.URL (:callback hub)) (.toURI))
-    (let [challenge (random-str 32)
-          req {"hub[mode]" (:mode hub) "hub[topic]" (:topic hub) "hub[challenge]" challenge}
-          resp (http/POST client (:callback hub) :body req)
-          challenge_check (http/string resp)
-          status (http/status resp)]
-      (if (and (= (:code status) 200) (= challenge challenge_check))
-        (do 
-          (if (= is_subscription true)
-            (push-create-subscription topic_id hub)
-            (push-delete-subscription topic_id hub))
-          success)
-        failure))))
+  (challenge-subscription [this topic_id hub is_subscription success failure]
+    (with-open [client (http/create-client)] ; Create client
+      (doto (new java.net.URL (:topic hub)) (.toURI))
+      (doto (new java.net.URL (:callback hub)) (.toURI))
+      (let [challenge (random-str 32)
+            req {"hub[mode]" (:mode hub) "hub[topic]" (:topic hub) "hub[challenge]" challenge}
+            resp (http/POST client (:callback hub) :body req)
+            challenge_check (http/string resp)
+            status (http/status resp)]
+        (if (and (= (:code status) 200) (= challenge challenge_check))
+          (do 
+            (if (= is_subscription true)
+              (create-subscription this topic_id hub)
+              (delete-subscription this topic_id hub))
+            success)
+          failure))))
 
-(defn push-fetch-required-feeds []
-  (let [date (date/minus (date/now) (date/minutes 15))]
+  (fetch-required-feeds [this]
+    (let [date (date/minus (date/now) (date/minutes 15))]
+      (sql/with-query-results results
+                              ["SELECT id, topic FROM topics WHERE last_fetched_at < ? OR last_fetched_at IS NULL" (postgres-date date)]
+                              (into [] results))))
+
+  (get-subscriptions [this topic_id]
     (sql/with-query-results results
-                            ["SELECT id, topic FROM topics WHERE last_fetched_at < ? OR last_fetched_at IS NULL" (postgres-date date)]
-                            (into [] results))))
+                            ["SELECT * FROM subscriptions WHERE topic_id = ?" topic_id]
+                            (into [] results)))
 
-(defn push-get-subscriptions [topic_id]
-  (sql/with-query-results results
-                          ["SELECT * FROM subscriptions WHERE topic_id = ?" topic_id]
-                          (into [] results)))
+  (distribute-feed-subscription [this feed-resp, subscription]
+    (with-open [client (http/create-client)]
+      (let [resp (http/POST client (:callback subscription) :body (http/string feed-resp))]
+          (http/await resp))))
 
-(defn push-distribute-feed-subscription [feed-resp, subscription]
-  (with-open [client (http/create-client)]
-    (let [resp (http/POST client (:callback subscription) :body (http/string feed-resp))]
-        (http/await resp))))
+  (distribute-feed [this feed]
+    (with-open [client (http/create-client)] ; Create client
+      (let [resp (http/GET client (:topic feed))
+            subscriptions (get-subscriptions this (:id feed))
+            resv (http/await resp)]
+        (for [subscription subscriptions]
+          (distribute-feed-subscription this resv subscription))
+        (mark-topic-fetched this feed))))
+)
 
-(defn push-distribute-feed [feed]
-  (with-open [client (http/create-client)] ; Create client
-    (let [resp (http/GET client (:topic feed))
-          subscriptions (push-get-subscriptions (:id feed))
-          resv (http/await resp)]
-      (for [subscription subscriptions]
-        (push-distribute-feed-subscription resv subscription))
-      (push-mark-feed-fetched feed))))
+(defn- make-push-server []
+  (PostgreSQLPushServer.)
+)
+
+(def ^{:private true} push-server (make-push-server))
 
 (defroutes routes
   (POST "/" {params :params} 
@@ -164,24 +170,24 @@
               (if (string/blank? (:topic hub))
                 {:status 400 :body "Topic is required for subscription/unsubscription"}
                 (wrap-connection
-                  (let [topic_row (push-get-or-create-topic (:topic hub))
+                  (let [topic_row (get-or-create-topic push-server (:topic hub))
                         topic_id (:id topic_row)]
-                    (if (and (= mode "subscription") (push-subscription-exists? topic_id (:callback hub)))
+                    (if (and (= mode "subscription") (subscription-exists? push-server topic_id (:callback hub)))
                       {:status 409 :body "Subscription already exists"}
-                      (push-challenge-subscription topic_id hub (= mode "subscribe")
+                      (challenge-subscription push-server topic_id hub (= mode "subscribe")
                                                    {:status 204 :body ""}
                                                    {:status 400 :body "Challenge was not accepted."}))))))
             (= mode "fetch")
             (wrap-connection
-              (let [feeds push-fetch-required-feeds]
-                (map push-distribute-feed feeds)
+              (let [feeds (fetch-required-feeds push-server)]
+                (map '(distribute-feed push-server) feeds)
                 {:status 202 :body "" }
               ))
             (= mode "publish") ; not finally implemented, because we don't act as a hub for now.
             (if (string/blank? (:topic hub))
               {:status 400 :body "Topic is required for publishing"}
               (wrap-connection
-                (let [topic-id (push-get-topic (:topic hub))]
+                (let [topic-id (get-topic push-server (:topic hub))]
                   (doto (new java.net.URL (:topic hub)) (.toURI))
                   (sql/with-query-results results
                                           ["SELECT * FROM subscriptions WHERE topic_id = ?" topic-id]
