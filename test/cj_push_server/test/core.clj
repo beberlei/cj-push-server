c3bb2c6b3630654761d3c10bb0d959e44f043c32
Benjamin Eberlei <kontakt@beberlei.de>
2012-10-04 12:30:03 +0200

    test subscription exists

--- test/cj_push_server/test/core.clj
+++ test/cj_push_server/test/core.clj
@@ -8,7 +8,7 @@
   (is (= "http://www.beberlei.de" (:topic (wrap-transaction (get-or-create-topic push-server "http://www.beberlei.de"))))))
 
 (deftest push-get-unknown-topic
-  (is (= nil (dbg (wrap-transaction (get-topic push-server "http://www.foobar.com/feed.xml"))))))
+  (is (= nil (wrap-transaction (get-topic push-server "http://www.foobar.com/feed.xml")))))
 
 (deftest push-mark-topic-fetched
     (wrap-transaction
@@ -16,3 +16,14 @@
         (mark-topic-fetched push-server feed)
         ))
     (is (not= nil (:last_fetched_at (wrap-transaction (get-topic push-server "http://www.foobar.com/feed2.xml"))))))
+
+(deftest subscription-not-exists
+  (is (= false (wrap-transaction (subscription-exists? push-server 1 "http://callback.com/1")))))
+
+(deftest subscription-exists
+  (wrap-transaction 
+    (let [callback (str "http://www.beberlei.de/" (random-str 16))
+          feed (get-or-create-topic push-server "http://www.foobar.com/feed3.xml")]
+      (create-subscription push-server (:id feed) {:callback callback})
+      (is (= true (wrap-transaction (subscription-exists? push-server (:id feed) callback)))))))
+  
